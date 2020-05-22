2f6169472dc1a368fdfa1d301611e10ca6078367
Steve Morris <steve@ajn.in>
2020-05-22 13:13:15 -0700

    add user input

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
-first_name = "11"
-print(first_name)
-print("These", "will be", "joined together by spaces")
+# first_name = "11"
+first_name = input("What is your fist name?  ")
+# print(first_name)
+# print("These", "will be", "joined together by spaces")
 
-print("Hello, Steve!")
+# print("Hello, Steve!")
 print("Hello,", first_name)
-print("Steve is learning Python")
+# print("Steve is learning Python")
 print(first_name, "is learning Python")
