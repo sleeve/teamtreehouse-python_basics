33027526db77d0c6adea46b6a1f924d5554938bc
Steve Morris <steve@ajn.in>
2020-05-22 10:46:39 -0700

    variables complete

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
-first_name = "Ada"
+first_name = "11"
 print(first_name)
 print("These", "will be", "joined together by spaces")
 
-print("Hello Steve!")
+print("Hello, Steve!")
+print("Hello,", first_name)
 print("Steve is learning Python")
+print(first_name, "is learning Python")
