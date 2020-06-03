ece084afa7507959c14a89ac851b5f9f59e9a32e
Steve Morris <steve@ajn.in>
2020-06-03 11:12:28 -0700

    start of conditional branching exercise

---
+++ 2.5-if_else_and_elif.py
@@ -0,0 +1,11 @@
+# If, Else and Elif
+
+## equality or comparison operator `==`
+fruit = "apples"
+print(fruit == "apples") # True
+print(fruit == "oranges") # False
+
+## import hello.py example
+first_name = input("What is your fist name?  ")
+print("Hello,", first_name)
+print(first_name, "is learning Python")
