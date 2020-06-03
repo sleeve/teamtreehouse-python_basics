e0460307c7cba0a66c45054d14a90b6a0b96c19d
Steve Morris <steve@ajn.in>
2020-06-03 11:27:45 -0700

    conditional branching

--- 2.5-if_else_and_elif.py
+++ 2.5-if_else_and_elif.py
@@ -8,4 +8,10 @@ print(fruit == "oranges") # False
 ## import hello.py example
 first_name = input("What is your fist name?  ")
 print("Hello,", first_name)
-print(first_name, "is learning Python")
+if first_name == "Steve":
+    print(first_name, "is learning Python")
+elif first_name == "Maximiliane":
+    print(first_name, "is learning with fellow students in the Community! Me too!")
+else:
+    print("You should totally learn Python, {}!".format(first_name))
+print("Have a great day {}!".format(first_name))
