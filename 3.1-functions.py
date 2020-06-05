90f362ff40a6f908deffbdca8edab44541b3d2fb
Steve Morris <steve@ajn.in>
2020-06-05 07:14:13 -0700

    complete functions exercise

--- 3.1-functions.py
+++ 3.1-functions.py
@@ -1,19 +1,11 @@
 # 3.1 Functions
 
-praise = "You are doing great"
-praise = praise.upper()
-number_of_characters = len(praise)
-result = praise + "!" * number_of_characters
-print(result)
+def yell(text):
+    text = text.upper()
+    number_of_characters = len(text)
+    result = text + "!" * (number_of_characters // 4)
+    print(result)
 
-advice = "Don't forget to ask for help"
-advice = advice.upper()
-number_of_characters = len(advice)
-result = advice + "!" * number_of_characters
-print(result)
-
-advice2 = "Don't Repeat Yourself. Keep things DRY"
-advice2 = advice2.upper()
-number_of_characters = len(advice2)
-result = advice2 + "!" * number_of_characters
-print(result)
+yell("You are doing great")
+yell("Don't forget to ask for help")
+yell("Don't Repeat Yourself. Keep things DRY")
