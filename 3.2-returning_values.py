90819be80e09fd321d983eab1895bf753efdfaa6
Steve Morris <steve@ajn.in>
2020-06-05 08:54:02 -0700

    error handling work

--- 3.2-returning_values.py
+++ 3.2-returning_values.py
@@ -5,9 +5,13 @@ import math
 def split_check(total, number_of_people):
     return math.ceil(total / number_of_people)
 
-total_due = float(input("What is the total? "))
-number_of_people = int(input("How many people? "))
-
-amount_due = split_check(total_due, number_of_people)
-
-print("Each person owes ${}".format(amount_due))
+try:
+    total_due = float(input("What is the total? "))
+    number_of_people = int(input("How many people? "))
+except ValueError:
+    print("Oh no! That's not a valid value. Try again...")
+#except ZeroDivisionError:
+    # TODO: write exception handling
+else:
+    amount_due = split_check(total_due, number_of_people)
+    print("Each person owes ${}".format(amount_due))
